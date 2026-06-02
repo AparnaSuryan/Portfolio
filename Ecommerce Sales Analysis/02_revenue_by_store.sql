fa0aca6d13c3d85f0269db1d227543165b428685
AparnaSuryan <43520306+AparnaSuryan@users.noreply.github.com>
2026-06-02 21:26:01 +0200

    Delete Ecommerce Sales Analysis/02_revenue_by_store.sql

--- Ecommerce Sales Analysis/02_revenue_by_store.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    ss_store_sk AS store,
-    ROUND(SUM(ss_net_paid), 2) AS total_revenue,
-    ROUND(SUM(ss_net_profit), 2) AS total_profit,
-    COUNT(DISTINCT ss_ticket_number) AS total_orders
-FROM SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL.STORE_SALES
-GROUP BY ss_store_sk
-ORDER BY total_revenue DESC
-LIMIT 20;
